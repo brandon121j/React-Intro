dd342eca5538088e0d975070b253d3ffb217c559
Brandon <brandon121j@gmail.com>
2021-10-18 15:38:29 -0400

    Addition of new folder and files

---
+++ src/components/MessageChild.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MessageChild extends Component {
+    render() {
+        return (
+            <div>
+                My favorite number is ${this.props.num}
+            </div>
+        )
+    }
+}
+
+export default MessageChild
