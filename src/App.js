466da257442f2fbce9b9c697b49abdf5ece69301
Brandon <brandon121j@gmail.com>
2021-10-19 12:05:31 -0400

    added App.js to Git

--- src/App.js
+++ src/App.js
@@ -3,6 +3,45 @@ import Message from "./components/Message";
 import "./App.css";
 
 export class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			count: 0,
+			toggleMe: false,
+			email: "",
+		};
+	}
+
+	add = () => {
+		this.setState({
+			count: this.state.count + 1,
+		});
+	};
+
+	subtract = () => {
+		this.setState({
+			count: this.state.count - 1,
+		});
+	};
+
+	toggleMe = () => {
+		// this.setState({
+		// 	toggleMe: !this.state.toggleMe
+		// });
+		this.setState((prevState) => {
+			return {
+				toggleMe: !prevState.toggleMe,
+			};
+		});
+	};
+
+	handleEmailChange = (e) => {
+		this.setState({
+			email: e.target.value
+		});
+	};
+
 	render() {
 		return (
 			// <div style={{ textAlign: "center" }}
@@ -10,6 +49,27 @@ export class App extends Component {
 				<div style={styles.appDiv}>
 					<h1>Brandon Johnson</h1>
 					<Message name="Brandon" message="how is your day going?" num={1} />
+					<hr />
+					<div>
+						Count: { this.state.count }
+						<div>
+							<button onClick={this.add}>+</button>
+							<button onClick={this.subtract}>-</button>
+						</div>
+					</div>
+					<div>
+						<div
+							style={{
+								height: 50,
+								backgroundColor: this.state.toggleMe ? "blue" : "red",
+							}}
+						>
+							<button onClick={this.toggleMe}>Toggle me</button>
+						</div>
+					</div>
+				</div>
+				<div>
+					<input name="email" value={this.state.email} onChange={this.handleEmailChange}></input>
 				</div>
 			</div>
 		);
@@ -23,12 +83,12 @@ const styles = {
 };
 
 const styles2 = {
-    divFontSize: {
-        fontSize: 50
-    },
-    divFontSizeInPercent: {
-        fontSize: "200%"
-    }
+	divFontSize: {
+		fontSize: 50,
+	},
+	divFontSizeInPercent: {
+		fontSize: "200%",
+	},
 };
 
 export default App;
