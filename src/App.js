620b1a15f4ce79f2067404fbc1649eaf35f7f877
Brandon <brandon121j@gmail.com>
2021-10-19 15:00:06 -0400

    Revamped app.js file

--- src/App.js
+++ src/App.js
@@ -1,199 +1,59 @@
 import React, { Component } from "react";
-import Message from "./components/Message";
 import "./App.css";
 
 
 export class App extends Component {
-	constructor(props) {
-		super(props);
+	state = {
+		postArray: [
+			{
+				id: 1,
+				body: "lol"
+			},
+			{
+				id: 2,
+				body: "hehe"
+			},
+			{
+				id: 3,
+				body: "haha"
+			}
+		],
+		post: "",
+	};
 
-		this.state = {
-			count: 0,
-			toggleMe: false,
-			email: "",
-			firstName: "",
-			lastName: "",
-			list: [
-				{
-					id: 1,
-					item: "bear"
-				},
-				{
-					id: 2,
-					item: "tiger"
-				},
-				{
-					id: 3,
-					item: "lion"
-				},
-			]
-		};
-	}
+	handleShowPostArray = () => (
+		<ul>
+			{ this.state.postArray.map(({body, id}) => (
+				<li key={id}>{body}</li>
+			))}
+		</ul>
+	);
 
-	add = () => {
+	handleInputChange = (e) => {
 		this.setState({
-			count: this.state.count + 1,
+			[e.target.name]: e.target.value
 		});
 	};
 
-	minus = () => {
-		this.setState({
-			count: this.state.count - 1,
-		});
+	handlePostSubmit = (e) => {
+		e.preventDefault();
+		console.log(this.state.post)
 	};
-
-	toggleMe = () => {
-		// this.setState({
-		//   toggleMe: !this.state.toggleMe,
-		// });
-
-		this.setState((prevState) => {
-			return {
-				toggleMe: !prevState.toggleMe,
-			};
-		});
-	};
-
-	handleEmailChange = (event) => {
-		this.setState({
-			email: event.target.value,
-		});
-	};
-
-	handleFirstNameChange = (event) => {
-		this.setState({
-			firstName: event.target.value,
-		});
-	};
-
-	handleOnSubmit = (event) => {
-		event.preventDefault();
-		console.log(this.state);
-	};
-
-	handleLastNameChange = (event) => {
-		this.setState({
-			lastName: event.target.value,
-		});
-	};
-
-	handleClickMe = () => {
-		console.log(this.state);
-	};
-
-	handleSecondFormSubmit = (event) => {
-		event.preventDefault();
-
-		console.log(this.state);
-	};
-
-	handleSecondFormOnChange = (event) => {
-		console.log(event.target.name, event.target.value);
-		this.setState({
-			[event.target.name]: event.target.value,
-		});
-	};
-
+	
 	render() {
+
+		const { post } = this.state;
 		return (
-			// <div className="App">
-			// <div style={styles.appDiv}>
 			<div style={{ textAlign: "center" }}>
-				<Message
-					name="class"
-					message="how is your day going?"
-					num={this.state.count}
-				/>
-				<hr />
-				<div>
-					Count: {this.state.count}
-					<div>
-						<button onClick={this.add}>+</button>
-						<button onClick={this.minus}>-</button>
-					</div>
-				</div>
-				<hr />
-				<div>
-					<div
-						style={{
-							height: 50,
-							backgroundColor: this.state.toggleMe ? "blue" : "red",
-						}}
-					>
-						<button onClick={this.toggleMe}>Toggle me</button>
-					</div>
-				</div>
-				<hr />
-				<div>
-					<input
-						name="email"
-						value={this.state.email}
-						onChange={this.handleEmailChange}
-					/>
-					<br />
-					{this.state.email}
-				</div>
-				<hr />
-				<form onSubmit={this.handleOnSubmit}>
-					<input
-						name="firstName"
-						value={this.state.firstName}
-						onChange={this.handleFirstNameChange}
-					/>
+				{this.handleShowPostArray()}
+				<form onSubmit={this.handlePostSubmit}>
+					<input name="post" value={post} onChange={this.handleInputChange} />
 					<button>Submit</button>
 				</form>
-				<hr />
-				<input
-					name="lastName"
-					value={this.state.lastName}
-					onChange={this.handleLastNameChange}
-				/>
-				<button onClick={this.handleClickMe}>Click Me</button>
-				<hr />
-				<form onSubmit={this.handleSecondFormSubmit}>
-					<input
-						name="email"
-						value={this.state.email}
-						onChange={this.handleSecondFormOnChange}
-					/>
-					<input
-						name="firstName"
-						value={this.state.firstName}
-						onChange={this.handleSecondFormOnChange}
-					/>
-					<input
-						name="lastName"
-						value={this.state.lastName}
-						onChange={this.handleSecondFormOnChange}
-					/>
-					<button>Form 2 Submit</button>
-				</form>
-				<hr/>
-
-				<ul>
-					{/* {
-						this.state.list.map((animals) => {
-							return(
-								<li>
-									{animals.item}
-								</li>
-							)
-						})
-					} */}
-
-					{this.state.list.map(({ item, id}) => (
-						<li key={id}>{item}</li>
-					))}
-				</ul>
 			</div>
 		);
 	}
 }
 
-const styles = {
-	appDiv: {
-		textAlign: "center",
-	},
-};
 
 export default App;
