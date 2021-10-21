84abb4a3a2042f7a68dd26e89ba1fb1d83d24ab2
Brandon <brandon121j@gmail.com>
2021-10-21 15:28:44 -0400

    Checkpoint commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
 import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
+
 import "./App.css";
 
 export class App extends Component {
-	state = {};
+    state = {
+        message: "....loading",
+        backgroundColor: "orange",
+        toggelShowMe: true
+    };
 
-	render() {
-		return <div className></div>
-	}
+    componentDidMount() {
+        //make ajax call to a server or an api
+
+        setTimeout(() => {
+        this.setState({
+            message: "component did mount executed! so the message is changed",
+            backgroundColor: "red",
+        });
+        }, 2000);
+    }
+
+    handleClickMe = () => {
+        this.setState({
+        message: "clicked",
+        });
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevState);
+        if (prevState.message !== this.state.message) {
+            setTimeout(() => {
+                this.setState({
+                backgroundColor: "blue",
+                message:
+                    "Component Did Update executed!!! so the message is changed again",
+                });
+            }, 2000);
+        }
+
+        // if (
+        //   prevState.message === "Component Did Update executed!!! so the message is changed again"
+        // ) {
+        //   console.log("hello class");
+        //   this.setState({ color: "pink" });
+        // }
+    }
+
+    toggelShowMe = () => {
+        this.setState((prevState) => {
+            return {
+                toggelShowMe : !prevState.toggelShowMe
+            }
+        })
+    }
+
+    render() {
+        return (
+        <div
+            className="App"
+            style={{ backgroundColor: this.state.backgroundColor }}
+        >
+            <h1>{this.state.message}</h1>
+            <button onClick={this.handleClickMe}>Click Me</button>
+            <hr />
+            <div style={{backgroundColor : "white"}}>
+                {this.state.toggelShowMe === true ? <ShowMe /> : ""}
+                <button onClick={this.toggelShowMe}>Toggle Me</button>
+            </div>
+            
+        </div>
+        );
+    }
+}
+
+class ShowMe extends Component{
+    state = {
+        showMeMessage : "Show me Component"
+    }
+
+    render(){
+        return <div>{this.state.showMeMessage}</div>
+    }
+        
 }
 
 export default App;
