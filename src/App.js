bb6bc88b3fc8aea0dfa5f31e3d8b67cb4f00eb65
Brandon <brandon121j@gmail.com>
2021-10-21 12:32:45 -0400

    Checkpoint commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,13 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
-
 export class App extends Component {
-	state = {
-		postArray: [
-			{
-				id: 1,
-				body: "lol"
-			},
-			{
-				id: 2,
-				body: "hehe"
-			},
-			{
-				id: 3,
-				body: "haha"
-			}
-		],
-		post: "",
-	};
+	state = {};
 
-	handleShowPostArray = () => (
-		<ul>
-			{ this.state.postArray.map(({body, id}) => (
-				<li key={id}>{body}</li>
-			))}
-		</ul>
-	);
-
-	handleInputChange = (e) => {
-		this.setState({
-			[e.target.name]: e.target.value
-		});
-	};
-
-	handlePostSubmit = (e) => {
-		e.preventDefault();
-		console.log(this.state.post)
-	};
-	
 	render() {
-
-		const { post } = this.state;
-		return (
-			<div style={{ textAlign: "center" }}>
-				{this.handleShowPostArray()}
-				<form onSubmit={this.handlePostSubmit}>
-					<input name="post" value={post} onChange={this.handleInputChange} />
-					<button>Submit</button>
-				</form>
-			</div>
-		);
+		return <div className></div>
 	}
 }
-
 
 export default App;
