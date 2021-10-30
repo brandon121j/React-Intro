ff9e30ad8a0d74b3b5d4d5b1644f8c471d0ac2f7
Brandon <brandon121j@gmail.com>
2021-10-30 19:06:38 -0400

    Fixed personal error

--- src/App-5.js
+++ src/App-5.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Pokemon from "./components/pokemon/Pokemon";
-import MainPokemon from "./components/MainPokemon/MainPokemon";
-import MainPokemonDetail from "./components/MainPokemon/MainPokemonDetail";
-
-import "./App.css";
-
+import "./App.css"
 export class App extends Component {
+
+
 	render() {
 		return (
 			<div className="App">
-				<Router>
-					<Switch>
-						<Route exact path="/abc" render={() => <h1>abc page</h1>} />
-						<Route exact path="/fetch-pokemon/:name" component={MainPokemonDetail} />
-						<Route exact path="/" component={MainPokemon} />
-						<Route render={() => <h1>Not found 404</h1>} />
-					</Switch>
-				</Router>
+				<Pokemon />
 			</div>
 		);
 	}
