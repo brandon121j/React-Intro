b972b25ca605efa24c89264fc37c0d218aa77d3b
Brandon <brandon121j@gmail.com>
2021-10-30 19:09:32 -0400

    Addition of MainPokemon folder and two new files within it

--- src/App.js
+++ src/App.js
@@ -8,24 +8,20 @@ import MainPokemonDetail from "./components/MainPokemon/MainPokemonDetail";
 import "./App.css";
 
 export class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/abc" render={() => <h1>abc page</h1>} />
-            <Route
-              exact
-              path="/fetch-pokemon/:name"
-              component={MainPokemonDetail}
-            />
-            <Route exact path="/" component={MainPokemon} />
-            <Route render={() => <h1>Not found 404</h1>} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Router>
+					<Switch>
+						<Route exact path="/abc" render={() => <h1>abc page</h1>} />
+						<Route exact path="/fetch-pokemon/:name" component={MainPokemonDetail} />
+						<Route exact path="/" component={MainPokemon} />
+						<Route render={() => <h1>Not found 404</h1>} />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
