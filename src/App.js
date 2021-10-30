1498980772958273d57174d1c77b641ee21086de
Brandon <brandon121j@gmail.com>
2021-10-30 19:17:16 -0400

    Changes finalized for today

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Pokemon from "./components/pokemon/Pokemon";
 import MainPokemon from "./components/MainPokemon/MainPokemon";
 import MainPokemonDetail from "./components/MainPokemon/MainPokemonDetail";
 
